050a0071efd303b39069aef20604ec9f2372f0f4
Tomas Gomez <tomasgomez616@gmail.com>
2023-03-18 16:06:21 -0300

    Primer Commit
    Página de empresa tecnológica que se centra en la creación de robots.

---
+++ js/function.js
@@ -0,0 +1,7 @@
+const boton1 = document.getElementsByClassName("botonTerminator")
+
+
+
+function alertaBoton () {
+    alert("ERROR no posee permiso para realizar esta acción. Contactando a la policía local.")
+}
